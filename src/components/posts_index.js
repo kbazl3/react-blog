ec58a752c30bfe2859f90a9d047dc47b3774d8ba
kbazl3 <kbarlowjs@gmail.com>
2018-05-20 22:59:59 -0600

    added fetchPosts action

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
+import {fetchPosts} from '../actions';
+import {connect} from 'react-redux';
 
 class PostsIndex extends Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
         return (
             <div>Posts Index</div>
         )
-
     }
 }
 
-export default PostsIndex
+
+
+export default connect(null, {fetchPosts})(PostsIndex) //passing in null because this container isn't managing any state
