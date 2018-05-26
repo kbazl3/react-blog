c62785c4ab6ca559203e96cbd6885016688daa38
kbazl3 <kbarlowjs@gmail.com>
2018-05-25 23:27:55 -0600

    added action creator for create post

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-        <Route path="/" component={PostsIndex}/>
+        <div>
+            <Switch>
+                <Route path="/posts/new" component={PostsNew}/>
+                <Route path="/" component={PostsIndex}/>
+            </Switch>
+        </div>
+
     </BrowserRouter>
 </Provider>, document.querySelector('.container'));
